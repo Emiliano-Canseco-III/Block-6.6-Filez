f785f0acfb9419ace1c554de2c02e0d91e67c699
Milo Canseco <canseco3rd@gmail.com>
2025-12-17 14:10:34 -0700

    Created seed function to seed tables with files and folders.

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,19 @@
 import db from "#db/client";
 
+import { createFolder } from "#db/queries/files.js";
+import { createFile } from "#db/queries/folders.js";
+
 await db.connect();
 await seed();
 await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  for (let i = 1; i <= 3; i++) {
+    await createFolder("Folder " + i);
+  }
+
+  for (let i = 1; i <= 5; i++) {
+    await createFile("Files " + i);
+  }
 }
