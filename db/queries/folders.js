2531ae509b72a1709e14ec5cb617c86246f94125
Milo Canseco <canseco3rd@gmail.com>
2025-12-17 16:00:00 -0700

    created two get functions, one returns all folders and the other returns a specific folder.

--- db/queries/folders.js
+++ db/queries/folders.js
@@ -13,3 +13,24 @@ export async function createFolder(name) {
   } = await db.query(sql, [name]);
   return folder;
 }
+
+export async function getFolders() {
+  const sql = `
+    SELECT *
+    FROM folders
+    `;
+  const { rows: folders } = await db.query(sql);
+  return folders;
+}
+
+export async function getFolderById(id) {
+  const sql = `
+    SELECT *
+    FROM folders
+    WHERE id = $1
+    `;
+  const {
+    rows: [folder],
+  } = await db.query(sql, [id]);
+  return folder;
+}
