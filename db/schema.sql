ed7bf850fdf3703efdc25194c3e9395a1aff215d
Milo Canseco <canseco3rd@gmail.com>
2025-12-17 14:43:23 -0700

    Added comma in files table.

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE files (
     id          serial PRIMARY KEY,
     name        text  NOT NULL,
     size        integer NOT NULL,
-    folder_id   integer NOT NULL REFERENCES folders(id) ON DELETE CASCADE
+    folder_id   integer NOT NULL REFERENCES folders(id) ON DELETE CASCADE,
 
     UNIQUE(name, folder_id)
 );
