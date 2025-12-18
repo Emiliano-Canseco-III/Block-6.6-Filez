1878472bbabe1e56640217aa5b51e815363f3136
Milo Canseco <canseco3rd@gmail.com>
2025-12-17 17:07:49 -0700

    Imported and mounted routers.

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 import express from "express";
+import folderRouter from "./api/folders.js";
+import fileRouter from "./api/files.js";
+
 const app = express();
+
+app.use(express.json());
+
+app.use("/files", fileRouter);
+app.use("/folders", folderRouter);
+
 export default app;
